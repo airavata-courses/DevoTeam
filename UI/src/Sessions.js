1ea1ca1542be8ecf9fb152cce9846c1b3778c96e
Manuja Sanjay Bandal <56208082+Manuja-B@users.noreply.github.com>
2020-02-10 11:48:29 -0500

    Update Sessions.js

--- UI/src/Sessions.js
+++ UI/src/Sessions.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import localStorage from "localStorage";
 
-export default class Session extends Component{
+export default class Sessions extends Component{
     constructor(props) {
         super(props);{
           this.state={
