becd9d4f987aa02d3d18c28b47de49e04f693e46
Sumant <34004948+Invictus17@users.noreply.github.com>
2020-02-12 13:01:38 -0500

    Changes while integrating UI with back-end

--- UI/src/Sessions.js
+++ UI/src/Sessions.js
@@ -2,74 +2,81 @@ import React, { Component } from "react";
 import { Form } from 'react-bootstrap';
 import axios from "axios";
 import localStorage from "localStorage";
+import {default as UUID} from "node-uuid";
 
 export default class Sessions extends Component{
     constructor(props) {
         super(props);{
-          this.state={
-            email:"",
-            year:"",
-            month:"",
-            day:"",
-            radar:"",
-            key:""
-          };
-          this.getSession = this.getSession.bind(this);
-          this.userInput = this.userInput.bind(this);
-    
+            this.state={
+                email:"",
+                year:"",
+                month:"",
+                day:"",
+                radar:"",
+                key:""
+            };
+            this.getSession = this.getSession.bind(this);
         }
-      }
-
-      getSession(){
+    }
+    componentWillMount() {
+        console.log("cdvbdskhv")
+       this.getSession();
+    }
+    getSession(){
         var postData = {
-          email: localStorage.getItem("currentUser")
+            email: localStorage.getItem("currentUser")
         };
+        console.log(postData)
         return axios({
-          method: "post",
-          url: "http://localhost:8080/getSession",
-          headers: { "Access-Control-Allow-Origin": "*" },
-          data: postData
+            method: "post",
+            url: "http://localhost:8081/sessions/fetch",
+            headers: { "Access-Control-Allow-Origin": "*" },
+            data: postData
         })
-          .then(response => {
-            this.setState({
-              /*yyyy:response.data.year,
-              mm:response.data.month,
-              dd:response.data.day,
-              radarst:response.data.radar,
-              time:response.data.updatedAt*/
-              output:response.data
+            .then(response => {
+
+                this.setState({
+                    output: JSON.stringify(response.data)
+                });
+            })
+            .catch(err => {
+                console.log(err);
             });
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
-render(){
-    return(
-<div>
-<p>
-   <h3><center> History of your previous sessions here</center></h3>
-   {this.state.output.map((el, i) =>(
-       <div>
-           style={{
-                    display: "inline-block",
-                    marginBottom: 18,
-                    marginRight: 18,
-                    marginLeft: 38,
-                    paddingTop: "0px",
-                    fontColor: "black"}}
-        >
+    }
 
-        <pre>Your input Year:{el.year}, Month:{el.month}, Day:{el.day}, Radar station:{el.radar}</pre>
-       {/*<Link to={{ pathname: "/SessionResult" }} style={{ float: "right", fontSize:"30px" }}>
-           Sessions
-           </Link>*/}
-       </div>
 
-   ))}
-   
-</p>
-</div>
-    );
+
+    render(){
+        console.log("eee:", this.state.output)
+        return(
+
+             <div>
+               <p>
+                     <h3><center> History of your previous sessions here</center></h3>
+                   {/*}  //          {this.state.output.map((el, i) =>(
+            //             <div>
+            //                 style={{
+            //                 display: "inline-block",
+            //                 marginBottom: 18,
+            //                 marginRight: 18,
+            //                 marginLeft: 38,
+            //                 paddingTop: "0px",
+            //                 fontColor: "black"}}
+            //                 >
+            //
+            //                 <pre>Your input Year:{el.year}, Month:{el.month}, Day:{el.day}, Radar station:{el.radar}</pre>
+            //
+            //
+            //             </div>
+
+                   // ))}
+
+
+{/*rgrg: {this.state.output[0].email}*/}
+
+                </p>
+            </div>
+
+        );
+    }
 }
-} 
