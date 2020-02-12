edf17642ec111cdc087f1880bd5078e10a780d67
Sumant <34004948+Invictus17@users.noreply.github.com>
2020-02-11 20:55:10 -0500

    Adding changes that came up while integrating UI with the system

--- UI/src/Home.js
+++ UI/src/Home.js
@@ -7,188 +7,207 @@ import axios from "axios";
 //import LandingPage from './LandingPage'
 import localStorage from "localStorage";
 export default class HomeComponent extends Component {
-  constructor(props) {
-    super(props);{
-      this.state={
-        year:"",
-        month:"",
-        day:"",
-        radar:"",
-        key:""
-      };
-      this.onChange = this.onChange.bind(this);
-      this.userInput = this.userInput.bind(this);
+    constructor(props) {
+        super(props);{
+            this.state={
+                year:"",
+                month:"",
+                day:"",
+                radar:"",
+                t_id:""
+            };
+            this.onChange = this.onChange.bind(this);
+            this.userInput = this.userInput.bind(this);
 
+        }
     }
-  }
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-      isChange: true
-    });
-  }
-  componentWillMount() {
- 
-    console.log("Key is",this.id);
-  }
-
-  userInput() {
-    if(ischange==true){
-         this.id = UUID.v4();
-    }
-    var inputData = {
-      year: this.state.year,
-      month: this.state.month,
-      day: this.state.day,
-      radar: this.state.radar,
-      key: this.id,
-      email: localStorage.getItem("currentUser")
-    };
-
-    return axios({
-      method: "post",
-      url: "http://localhost:8081/weather",
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-
-      },
-      data: inputData
-    })
-      .then(response => {
+    onChange(e) {
         this.setState({
-          user: response.data,
-          spectrum_width: response.data.max_spectrum_width,
-          plot: response.data.plot,
+            [e.target.name]: e.target.value,
+            isChange: true
         });
-       /* if (response["data"] == "success") {
-       console.log("Success");}*/ 
 
-      })
-      .catch(err => {});
-  }
 
-  
+    }
+    componentWillMount() {
+        console.log(this.state.id)
+    }
 
-  render() {
-    let imgurl="data:image/png;base64, "+{this.state.plot}
-    if (localStorage.getItem("isLogin") == "true") {
-    return (
-      <React.Fragment>
-       
-        <div>
-          <br></br>
-          <br></br>
-          <h4><center>Find your weather</center></h4>
-          <Form>
-          <Link to={{ pathname: "/Sessions" }} style={{ float: "right", fontSize:"30px" }}>
-           Sessions
-          </Link>
-                  <div style={{ padding: "3%" }}>
-                    <fieldset className="form-group">
-                      <br></br>
-                      <p
-                        style={{
-                          color: "#0277bd",
-                          marginBottom: "0px",
-                          fontSize: "20px"
-                        }}
-                      >
-                        Enter Year:
-                      </p>
-                      <input
-                        className="form-control"
-                        type="text"
-                        name="year"
-                        value={this.state.year}
-                        onChange={this.onChange}
-                        
-                      />
-                      <br></br>
-                      <p
-                        style={{
-                          color: "#0277bd",
-                          marginBottom: "0px",
-                          fontSize: "20px"
-                        }}
-                      >
-                        Enter Month:
-                      </p>
-                      <input
-                        className="form-control"
-                        type="text"
-                        name="month"
-                        value={this.state.month}
-                        onChange={this.onChange}
-                       
-                      />
-                      <br></br>
-                      <p
-                        style={{
-                          color: "#0277bd",
-                          marginBottom: "0px",
-                          fontSize: "20px"
-                        }}
-                      >
-                        Enter Day:
-                      </p>
-                      <input
-                        className="form-control"
-                        type="text"
-                        name="day"
-                        value={this.state.day}
-                        onChange={this.onChange}
-                       
-                      />
-                      <br></br>
-                      <p
-                        style={{
-                          color: "#0277bd",
-                          marginBottom: "0px",
-                          fontSize: "20px"
-                        }}
-                      >
-                        Radar station:
-                      </p>
-                      <input
-                        className="form-control"
-                        type="text"
-                        name="radar"
-                        value={this.state.radar}
-                        onChange={this.onChange}
-                        
-                      />
-                    </fieldset>
-                    <button
-                      className="btn btn-success"
-                      style={{ float: "right" }}
-                      type="button"
-                      onClick={this.userInput}
-                    >
-                      Find
-                    </button>
-                    <br></br>
-                  </div>
-                </Form>
+    userInput() {
+        if(this.state.isChange==true) {
+            this.setState({
+                isChange: false
+            })
+            this.id = UUID.v4();
+        }
+        console.log("Key on find",this.id)
+        var inputData = {
+            year: this.state.year,
+            month: this.state.month,
+            day: this.state.day,
+            radar: this.state.radar,
+            t_id: this.id,
+            email: localStorage.getItem("currentUser")
+        };
+
+        return axios({
+            method: "post",
+            url: "http://localhost:8081/weather",
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+
+            },
+            data: inputData
+        })
+            .then(response => {
+                console.log("Plot image:", response.data)
+                this.setState({
+                    data: response.data,
+                    spectrum_width: response.data.max_spectrum_width,
+                    plot: response.data.plot,
+                });
+                if (response["data"] == "Sent user input to micro-services")
+                {
+                    render:
+                    {
+                        return (
+                            <div>
+                                <h2>  {this.state.data}</h2>
+                            </div>
+                        );
+                    }
+                }
+
+            })
+            .catch(err => {});
+    }
+
+
+
+    render() {
+        let imgurl = "data:image/png;base64, "+this.state.plot
+        if (localStorage.getItem("isLogin") == "true") {
+            return (
+                <React.Fragment>
+
+                    <div>
+                        <br></br>
+                        <br></br>
+                        <h4><center>Find your weather</center></h4>
+                        <Form>
+                            <Link to={{ pathname: "/Sessions" }} style={{ float: "right", fontSize:"30px" }}>
+                                Sessions
+                            </Link>
+                            <div style={{ padding: "3%" }}>
+                                <fieldset className="form-group">
+                                    <br></br>
+                                    <p
+                                        style={{
+                                            color: "#0277bd",
+                                            marginBottom: "0px",
+                                            fontSize: "20px"
+                                        }}
+                                    >
+                                        Enter Year:
+                                    </p>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="year"
+                                        value={this.state.year}
+                                        onChange={this.onChange}
+
+                                    />
+                                    <br></br>
+                                    <p
+                                        style={{
+                                            color: "#0277bd",
+                                            marginBottom: "0px",
+                                            fontSize: "20px"
+                                        }}
+                                    >
+                                        Enter Month:
+                                    </p>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="month"
+                                        value={this.state.month}
+                                        onChange={this.onChange}
+
+                                    />
+                                    <br></br>
+                                    <p
+                                        style={{
+                                            color: "#0277bd",
+                                            marginBottom: "0px",
+                                            fontSize: "20px"
+                                        }}
+                                    >
+                                        Enter Day:
+                                    </p>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="day"
+                                        value={this.state.day}
+                                        onChange={this.onChange}
+
+                                    />
+                                    <br></br>
+                                    <p
+                                        style={{
+                                            color: "#0277bd",
+                                            marginBottom: "0px",
+                                            fontSize: "20px"
+                                        }}
+                                    >
+                                        Radar station:
+                                    </p>
+                                    <input
+                                        className="form-control"
+                                        type="text"
+                                        name="radar"
+                                        value={this.state.radar}
+                                        onChange={this.onChange}
+
+                                    />
+                                </fieldset>
+                                <button
+                                    className="btn btn-success"
+                                    style={{ float: "right" }}
+                                    type="button"
+                                    onClick={this.userInput}
+                                >
+                                    Find
+                                </button>
+                                <br></br>
+                            </div>
+                        </Form>
+                        <div>
+
+
+                            <img src={imgurl}/>
+
+
+                        </div>
+                    </div>
+                </React.Fragment>
+            );
+        }
+
+        else{
+            return(
                 <div>
-                  <img src={imgurl} alt="Red dot"/>
-                </div>
-        </div>
-      </React.Fragment>
-    );
-  }
 
-else{
-  return(
-    <div>
-   
-    
-          <br></br>
-          <br></br>
-          <h4><center>Login to get the weather data</center></h4>
-        
-     
-        </div>
-  );
-}
-  }
+
+                    <br></br>
+                    <br></br>
+                    <h4><center>Login to get the weather data</center></h4>
+
+
+                </div>
+            );
+        }
+    }
 }
