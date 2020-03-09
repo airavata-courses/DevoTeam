955abbc13299c602d5e32eb56a3ff3e82533bd5d
Sainath Parakala <sainath256@gmail.com>
2020-03-08 21:17:43 -0400

    Dockerized session and added MongoDB

--- API_gateway/api/routes/sessions.js
+++ API_gateway/api/routes/sessions.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 
-// router.post ('/save', async (req, res, next) => {
-//     const response = await axios.post('http://localhost:9000/save', {...req.body} );
-//     const data =
-//     console.log(response.config.data);
-//     res.status('201').send('saved');
-// });
+router.post ('/save', async (req, res, next) => {
+    const response = await axios.post('http://session:9000/save', {...req.body} );
+    const data =
+    console.log(response.config.data);
+    res.status('201').send('saved');
+});
 
 router.post('/fetch', async(req, res, next) => {
-    const response = await axios.post('http://localhost:9000/fetch', {...req.body} );
+    const response = await axios.post('http://session:9000/fetch', {...req.body} );
     const data = response.data;
     console.log(response.data);
     res.status('200').send(data);
