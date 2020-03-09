277658e1a30cc58c7554ad5b0fefa80f35841ef6
Sumant <34004948+Invictus17@users.noreply.github.com>
2020-03-09 15:40:44 -0400

    Update create_master.sh

--- Jetstream_create_instance/create_master.sh
+++ Jetstream_create_instance/create_master.sh
@@ -1,9 +1,9 @@
-openstack server create Devoteam-master --flavor m1.small --image 0d13b72c-db35-439f-9118-2946148eb5c6 --key-name ${OS_USERNAME}-api-key --security-group ${OS_USERNAME}-global-ssh --nic net-id=${OS_USERNAME}-api-net
+openstack server create <master_alias> --flavor m1.small --image 0d13b72c-db35-439f-9118-2946148eb5c6 --key-name ${OS_USERNAME}-api-key --security-group ${OS_USERNAME}-global-ssh --nic net-id=${OS_USERNAME}-api-net
 
 openstack floating ip create public
 
 echo "Please enter your floating ip address:"
 read -sr FLOATING_IP
-openstack server add floating ip Devoteam-master $FLOATING_IP
+openstack server add floating ip <master_alias> $FLOATING_IP
 
 ssh -i ${OS_USERNAME}-api-key ubuntu@$FLOATING_IP
